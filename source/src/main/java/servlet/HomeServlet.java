a9f59b8febce120c7b48823fef09713b1b7900b0
plusdojo2026-nose <nose-konoha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 13:04:51 +0900

    Update HomeServlet.java

--- source/src/main/java/servlet/HomeServlet.java
+++ source/src/main/java/servlet/HomeServlet.java
@@ -1,5 +1,41 @@
 package servlet;
 
-public class HomeServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	//直接servletを実行したときに動くメソッド（リダイレクトもこっちが動く）
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*
+	
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/webappAns/LoginServlet");
+			return;
+		}*/
+		
+		//ここで色んな処理をする（daoに処理を依頼して、requestにセットしたりする）
+		
+		
+
+		// メニューページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
+		dispatcher.forward(request, response);
+	}
+	//<form method = "POST" でservletを指定したときに動くメソッド
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
 
 }
