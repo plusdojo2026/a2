9d557740cc04de2dee1b74e1b585f37823a30e13
plusdojo2026-nose <nose-konoha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 13:04:45 +0900

    Update StoragesDao.java

--- source/src/main/java/dao/StoragesDao.java
+++ source/src/main/java/dao/StoragesDao.java
@@ -1,5 +1,13 @@
 package dao;
 
-public class StoragesDao {
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
+import dto.Storage;
+
+public class StoragesDao {
+	public List<Storage> select(Storage ???) {
+		Connection conn = null;
+		List<Storage> cardList = new ArrayList<Storage>();
 }
