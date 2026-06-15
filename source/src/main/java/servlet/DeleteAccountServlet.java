12d30b936885ebe43719a684cb7d827d94619814
plusdojo2026-furumoto <furumoto-keigo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 16:41:37 +0900

    Update DeleteAccountServlet.java

--- source/src/main/java/servlet/DeleteAccountServlet.java
+++ source/src/main/java/servlet/DeleteAccountServlet.java
@@ -1,5 +1,45 @@
 package servlet;
 
-public class DeleteAccountServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DeleteAccountServlet")
+public class DeleteAccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public DeleteAccountServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+
+//		HttpSession session = request.getSession();
+		
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/a2/LoginServlet");
+//			return;
+//		}
+//		//DAOが出来たら変える。
+//		UsersDao userDao = new UsersDao();
+//		List<User> userinfo = userDao.info(new User());
+		
+	
+//	
+//		// 検索結果をリクエストスコープに格納する
+//		request.setAttribute("userinfo", userinfo);
+		
+		
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/delete_account.jsp");
+		dispatcher.forward(request, response);
+	}
 
 }
