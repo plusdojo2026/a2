ce1b9b80a28f40f73f2a4bc2016c8c7ec8496e92
plusdojo2026-furumoto <furumoto-keigo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 11:59:27 +0900

    Update MyPageServlet.java

--- source/src/main/java/servlet/MyPageServlet.java
+++ source/src/main/java/servlet/MyPageServlet.java
@@ -1,5 +1,38 @@
 package servlet;
 
-public class MyPageServlet {
+
+
+@WebServlet("/MyPageServlet")
+public class ListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public MyPageServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/a2/LoginServlet");
+			return;
+		}
+		
+		UserDAO userDao = new UserDAO();
+		List<User> userinfo = userDao.list(new User());
+		
+	
+	
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("userinfo", userinfo);
+		
+		
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
+		dispatcher.forward(request, response);
+	}
 
 }
