db34e5f778edcd70bdf7e8b72bd100971e0248e2
plusdojo2026-nose <nose-konoha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 17:39:26 +0900

    Update StoragesDao.java

--- source/src/main/java/dao/StoragesDao.java
+++ source/src/main/java/dao/StoragesDao.java
@@ -1,65 +1,57 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
-import java.util.List;
-
-import dto.Storage;
-
 public class StoragesDao {
-	public List<Storage> select(Storage ???) {
-		Connection conn = null;
-		List<Storage> cardList = new ArrayList<Storage>();
-		
-		
-		try {
-			
-			// JDBCドライバを読み込む データベース接続した
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a2"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
-			
-			
-			
-			
-			// SQL文を準備する
-			String sql = "SELECT"
-					+ "storage_id, "
-					+"user_id, "
-					+"weight, "
-					+"fat, "
-					+"memo, "
-					+"stamp, "
-					+"date, "
-					+"id, "
-					+"tr_id, "
-					+"tr_weight, "
-					+"counts, "
-					+"sets, "
-					+ "FROM storages"
-					+ "WHERE storage_id LIKE ?" 
-					+ "AND user_id LIKE ? "
-					+ "AND weight LIKE ?"
-					+ "AND fat LIKE ?"
-					+ "AND memo LIKE ?"
-					+ "AND stamp LIKE ?"
-					+ "AND date LIKE ?"
-					+ "AND id LIKE ?"
-					+ "AND tr_id LIKE ?"
-					+ "AND tr_weight LIKE ?"
-					+ "AND counts LIKE ?"
-					+ "AND sets LIKE ?"
-					+ "ORDER BY storage_id";
-					
-			PreparedStatement pStmt = conn.prepareStatement(sql);	
-			
-						
-		}
+//	public List<Storage> select(Storage ) {
+//		Connection conn = null;
+//		List<Storage> cardList = new ArrayList<Storage>();
+//		
+//		
+//		try {
+//			
+//			// JDBCドライバを読み込む データベース接続した
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			
+//			// データベースに接続する
+//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a2"
+//					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+//					"root", "password");
+//			
+//			
+//			
+//			
+//			// SQL文を準備する
+//			String sql = "SELECT"
+//					+ "storage_id, "
+//					+"user_id, "
+//					+"weight, "
+//					+"fat, "
+//					+"memo, "
+//					+"stamp, "
+//					+"date, "
+//					+"id, "
+//					+"tr_id, "
+//					+"tr_weight, "
+//					+"counts, "
+//					+"sets, "
+//					+ "FROM storages"
+//					+ "WHERE storage_id LIKE ?" 
+//					+ "AND user_id LIKE ? "
+//					+ "AND weight LIKE ?"
+//					+ "AND fat LIKE ?"
+//					+ "AND memo LIKE ?"
+//					+ "AND stamp LIKE ?"
+//					+ "AND date LIKE ?"
+//					+ "AND id LIKE ?"
+//					+ "AND tr_id LIKE ?"
+//					+ "AND tr_weight LIKE ?"
+//					+ "AND counts LIKE ?"
+//					+ "AND sets LIKE ?"
+//					+ "ORDER BY storage_id";
+//					
+//			PreparedStatement pStmt = conn.prepareStatement(sql);	
+//			
+//						
+//		}
 						
 	}
-}
+//}
