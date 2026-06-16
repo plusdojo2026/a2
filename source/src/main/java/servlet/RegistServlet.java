52722466d650866496854cafdd4dfcc4474c875c
plusdojo2026-kobayashiharuno <kobayashi-haruno-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 16:43:51 +0900

    Update RegistServlet.java

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -1,20 +1,18 @@
 package servlet;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+import javax.naming.spi.DirStateFactory.Result;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+impor
 import dao.UsersDao;
-import dto.User;
-@WebServlet("/RegistServlet")
+
 public class RegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,10 +20,10 @@ public class RegistServlet extends HttpServlet {
 		throws ServletException, IOException {
 	// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/webapp/LoginServlet");
-//			return;
-//		}
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/webapp/LoginServlet");
+			return;
+	}
 
 		// 登録ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
@@ -36,25 +34,22 @@ public class RegistServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/webapp/LoginServlet");
-//			return;
-//	}
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/webapp/LoginServlet");
+			return;
+	}
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		String userId = request.getParameter("user_id");/* <input type="text" name="xxxx"←この部分> */
+		String userID = request.getParameter("userID");/* <input type="text" name="xxxx"←この部分> */
 		String password = request.getParameter("password");
-		String userName = request.getParameter("user");
+		String username = request.getParameter("user");
 		String gender = request.getParameter("gender");
-		Double height = Double.parseDouble(request.getParameter("height")); 
-		Double targetWeight = Double.parseDouble(request.getParameter("target_weight"));
-		//今日の日付を取得
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		String dateTime = now.format(formatter);
+		String height = request.getParameter("height");
+		String weight = request.getParameter("weight");
 		
 		// 登録処理を行う
 		UsersDao uDao = new UsersDao();
+<<<<<<< HEAD
 
 		User us = new User(0,  userName,  height,  gender,  targetWeight,  0,
 				 userId,  password,  0,  0,  0,  dateTime);
@@ -68,7 +63,16 @@ public class RegistServlet extends HttpServlet {
 			// 結果ページにフォワードする
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
 			dispatcher.forward(request, response);
+=======
+		if (uDao.insert(new User(0, userID, password, username, gender, height, weight))) { // 登録成功
+			request.setAttribute("result", new Result("登録成功！", "レコードを登録しました。", "/webapp/HomeServlet"));
+		} else { // 登録失敗
+			request.setAttribute("result", new Result("登録失敗！", "レコードを登録できませんでした。", "/webapp/MenuServlet"));
+>>>>>>> 216bd73d8cd496d348048bd10228d5aa12b421d6
 		}
-		
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+		dispatcher.forward(request, response);
 	}
 }
