6d19da0f159bd61b717513c26a2e44a147b303e1
plusdojo2026-kobayashiharuno <kobayashi-haruno-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 10:38:41 +0900

    r

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.UsersDao;
 import dto.User;
@@ -20,11 +19,7 @@ public class RegistServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-	// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/webapp/LoginServlet");
-//			return;
+
 //		}
 
 		// 登録ページにフォワードする
@@ -34,12 +29,7 @@ public class RegistServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/webapp/LoginServlet");
-//			return;
-//	}
+		
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String userId = request.getParameter("user_id");/* <input type="text" name="xxxx"←この部分> */
--- source/src/main/webapp/WEB-INF/jsp/regist.jsp
+++ source/src/main/webapp/WEB-INF/jsp/regist.jsp
@@ -35,10 +35,10 @@
 	<p style="color:red">${result}</p> <!-- home.jspにもこれを書いたら登録完了の表示ができるよ -->
 	
 
-</form>
-<input type="button"  onclick="re()" value="リセット">
-<input type="submit" onclick="location.href='home.jsp'" value="登録">
 
+<input type="button"  onclick="re()" value="リセット">
+<input type="submit"  value="登録">
+</form>
 </main>
 <!--　メインここまで　-->
 <!--　フッターここから　-->
