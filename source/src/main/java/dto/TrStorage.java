963238a83491d51eefeab269b9a3338fa46c147f
plusdojo2026-furumoto <furumoto-keigo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 16:29:27 +0900

    フレンドページ

--- source/src/main/java/dto/TrStorage.java
+++ source/src/main/java/dto/TrStorage.java
@@ -5,16 +5,17 @@ import java.util.Date;
 
 //フレンド一覧のフレンドのTr内容表示用のDTO
 public class TrStorage  implements Serializable{
-	private String userId;
+	private String friendUserId;
 	private int trId;
 	private int trWeight;
 	private int count;
 	private int sets;
 	private String memo;
 	private Date date;
-	public TrStorage(String userId, int trId, int trWeight, int count, int sets, String memo, Date date) {
+	
+	public TrStorage(String friendUserId, int trId, int trWeight, int count, int sets, String memo, Date date) {
 		super();
-		this.userId = userId;
+		this.friendUserId = friendUserId;
 		this.trId = trId;
 		this.trWeight = trWeight;
 		this.count = count;
@@ -22,53 +23,67 @@ public class TrStorage  implements Serializable{
 		this.memo = memo;
 		this.date = date;
 	}
+
 	public TrStorage() {
 		super();
 	}
-	
-	
-	public String getUserId() {
-		return userId;
+
+	public String getFriendUserId() {
+		return friendUserId;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
+
+	public void setFriendUserId(String friendUserId) {
+		this.friendUserId = friendUserId;
 	}
+
 	public int getTrId() {
 		return trId;
 	}
+
 	public void setTrId(int trId) {
 		this.trId = trId;
 	}
+
 	public int getTrWeight() {
 		return trWeight;
 	}
+
 	public void setTrWeight(int trWeight) {
 		this.trWeight = trWeight;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
 	public int getSets() {
 		return sets;
 	}
+
 	public void setSets(int sets) {
 		this.sets = sets;
 	}
+
 	public String getMemo() {
 		return memo;
 	}
+
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	
 	
 	
 }
