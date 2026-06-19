66d22d451945159079b3e873e2f8b73162ebc32e
plusdojo2026-furumoto <furumoto-keigo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 17:54:08 +0900

    フレンド

--- source/src/main/java/servlet/FriendListServlet.java
+++ source/src/main/java/servlet/FriendListServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.gson.Gson;
+
 import dao.FriendsDao;
 import dto.Friend;
 import dto.TrStorage;
