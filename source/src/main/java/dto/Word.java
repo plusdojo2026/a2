d5bfb10840645aa8f5050dedc7bc6eff0f56b562
plusdojo2026-oikawa <oikawa-rina-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-20 15:08:40 +0900

    情報ページ

--- source/src/main/java/dto/Word.java
+++ source/src/main/java/dto/Word.java
@@ -1,5 +1,32 @@
 package dto;
 
-public class Word {
+import java.io.Serializable;
 
+public class Word implements Serializable{
+	
+	private int Word;
+	private String WordOfDay;
+	
+	// コンストラクタ
+	public Word(int word, String wordOfDay) {
+		super();
+		Word = word;
+		WordOfDay = wordOfDay;
+	}
+	
+	// 以下ゲッタセッタ
+	protected int getWord() {
+		return Word;
+	}
+	protected void setWord(int word) {
+		Word = word;
+	}
+	protected String getWordOfDay() {
+		return WordOfDay;
+	}
+	protected void setWordOfDay(String wordOfDay) {
+		WordOfDay = wordOfDay;
+	}
+	
+	
 }
